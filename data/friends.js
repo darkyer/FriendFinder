9fb1fbc22f2073eb6fcf3aac506811fc484eca64
Ernesto Solares <ernesto.solares.guerrero@gmail.com>
2019-05-26 20:58:07 -0500

    basic funcionality added + added gitignore and removed node modules

---
+++ data/friends.js
@@ -0,0 +1,14 @@
+var friends = [
+    {
+        name: "Don Ramon",
+        photo: "http://d1poh340f4imgl.cloudfront.net/upload/images/534x326/2015/09/02/6ea7e16ca99ef409f6c096a89d591232_534x326.jpg",
+        scores: [1, 1, 3, 4, 1, 5, 5, 2, 1, 1]
+    },
+    {
+        name: "Johnny Bravo",
+        photo: "https://i.pinimg.com/736x/9e/04/45/9e0445eff4ecefcbf590980204db14cd.jpg",
+        scores: [4, 5, 2, 1, 1, 1, 4, 2, 5, 5]
+    }
+];
+
+module.exports = friends;
