4d07f5c06e38d687f206dfa821a67fbd3a57f682
Ernesto Solares <ernesto.solares.guerrero@gmail.com>
2019-05-31 21:42:21 -0500

    added more friends option

--- data/friends.js
+++ data/friends.js
@@ -8,6 +8,21 @@ var friends = [
         name: "Johnny Bravo",
         photo: "https://i.pinimg.com/736x/9e/04/45/9e0445eff4ecefcbf590980204db14cd.jpg",
         scores: [4, 5, 2, 1, 1, 1, 4, 2, 5, 5]
+    },
+    {
+        name: "Dexter",
+        photo: "http://www.media2.hw-static.com/media/2016/08/cartoonnetwork_dexterslabratory_083116_1800x1200-1800x1200.jpg",
+        scores: [1, 3, 2, 4, 2, 4, 4, 1, 1, 5]
+    },
+    {
+        name: "Juan Gabriel",
+        photo: "https://www.elsiglodetorreon.com.mx/m/i/2018/11/1118268.jpeg",
+        scores: [1, 1, 2, 1, 3, 1, 2, 2, 4, 3]
+    },
+    {
+        name: "Ni Mergas",
+        photo: "http://www.quickmeme.com/img/58/589b6ac6b8e607b1dfee8683f5562927638e994b13cba224a08130b63363da82.jpg",
+        scores: [5, 5, 2, 1, 4, 3, 4, 2, 5, 5]
     }
 ];
 
